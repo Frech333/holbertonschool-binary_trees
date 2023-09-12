981c9e6e3a5bedf5124b23159060e105aef26e0a
Frech333 <josefrech@gmail.com>
2023-09-11 18:24:23 -0600

    C Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,9 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+
 /* Other typedefs */
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
